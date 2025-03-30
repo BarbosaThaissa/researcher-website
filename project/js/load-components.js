272d7dd408bd3a4573989c2bc20361b0521f20ad
Barbosa Thaíssa <thathamaria03@gmail.com>
2025-03-30 07:32:32 -0300

    feat: esquele header e footer e load components deles

---
+++ project/js/load-components.js
@@ -0,0 +1,20 @@
+// Função para carregar componentes
+async function includeHTML(id, file) {
+  const element = document.getElementById(id);
+  try {
+    const response = await fetch(file);
+    if (response.ok) {
+      element.innerHTML = await response.text();
+    }
+  } catch (err) {
+    console.error("Erro ao carregar o arquivo: ", err);
+
+    element.innerHTML = "<p>Erro ao carregar o conteúdo.</p>";
+  }
+}
+
+// Carrega o header e footer quando a página é carregada
+document.addEventListener("DOMContentLoaded", () => {
+  includeHTML("header", "./components/header.html");
+  includeHTML("footer", "./components/footer.html");
+});
